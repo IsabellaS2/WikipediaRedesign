94f24810e54a1684b490910d315dccda20453f77
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-08-31 11:44:26 +0100

    Create home page with designs

--- new-react-vite/src/Home.jsx
+++ new-react-vite/src/Home.jsx
@@ -9,15 +9,48 @@ function App() {
       <Navbar />
 
       <div className="homeContainer">
-        <h1 className="mainHeadertitle">Wikipedia</h1>
-        <h3>Welcome to this page</h3>
-        <div className="homecards">
-          <HomeCards
-            imgSrc="./new-react-vite/public/assets/apples.jpg"
-            title="Apples"
-            text="Apples are a popular and nutritious fruit from the apple tree known for their crisp texture and sweet or tart flavor."
-            link="#"
-          />{" "}
+        <div className="leftContainer">
+          <h1 className="mainHeadertitle">Wikipedia</h1>
+          <h3>Connecting You to the World’s Knowledge, One Search at a Time</h3>
+          <div className="searchSection">
+            <input
+              className="searchBar"
+              type="text"
+              placeholder="Searching Wikipedia..."
+            />
+            <a href="#">
+              <img className="searchIcon" src="./new-react-vite/public/assets/searchIcon.png" alt="" />
+            </a>
+          </div>
+        </div>
+
+        <div className="globeIconContainer">
+        <img
+            className="globeIcon"
+            src="./new-react-vite/public/assets/globeIcon.png" /* Add the correct path to your globe icon */
+            alt="Globe Icon"
+          />
+
+        </div>
+
+        <div className="homeCardSection">
+          <div className="topCard">
+            <HomeCards
+              imgSrc="./new-react-vite/public/assets/apples.jpg"
+              title="Apples"
+              text="Apples are a popular and nutritious fruit from the apple tree known for their crisp texture and sweet or tart flavor."
+              link="#"
+            />{" "}
+          </div>
+
+          <div className="bottomCard">
+            <HomeCards
+              imgSrc="./new-react-vite/public/assets/apples.jpg"
+              title="Apples"
+              text="Apples are a popular and nutritious fruit from the apple tree known for their crisp texture and sweet or tart flavor."
+              link="#"
+            />{" "}
+          </div>
         </div>
       </div>
     </>
